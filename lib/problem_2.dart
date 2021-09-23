6926754bd6cd1a5e61967e4f0a795a03140c028a
Korppi <3602291+Korppi@users.noreply.github.com>
2021-09-23 11:58:31 +0300

    solved problem 2

--- lib/problem_2.dart
+++ lib/problem_2.dart
@@ -2,8 +2,23 @@ import 'package:http/http.dart' as http;
 
 /// http://www.pythonchallenge.com/pc/def/ocr.html
 ///
-/// answer
+/// answer equality
 
-void main() {
-  ///
+Future<void> main() async {
+  var url = Uri.parse('http://www.pythonchallenge.com/pc/def/ocr.html');
+  var response = await http.get(url);
+  var start =
+      response.body.lastIndexOf('<!--') + 4; // +4 so we skip starting <!--
+  var end = response.body.lastIndexOf('-->');
+  var messyString = response.body.substring(start, end);
+  // var charCounts = SortedMap(Ordering.byValue());
+  var charCounts = <String, int>{};
+  messyString = messyString.trim(); //
+  messyString.split('').forEach((char) {
+    charCounts.update(char, (value) => ++value, ifAbsent: () => 0);
+  });
+  // i used sortermap before and found out that there are some letters
+  // which are rare (they appear only once) so i do this removeWhere
+  charCounts.removeWhere((key, value) => value > 1);
+  print('${charCounts.keys.toString()}');
 }
